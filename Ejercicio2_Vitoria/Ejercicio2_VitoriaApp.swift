acd647683fee5d7916c95dc6497865fb1542ccf9
vitxivito <133760252+vitxivito@users.noreply.github.com>
2024-11-28 19:42:23 +0100

    Initial Commit

---
+++ Ejercicio2_Vitoria/Ejercicio2_VitoriaApp.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio2_VitoriaApp.swift
+//  Ejercicio2_Vitoria
+//
+//  Created by Vitoria Garcia on 28/11/24.
+//
+
+import SwiftUI
+
+@main
+struct Ejercicio2_VitoriaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Ejercicio2_VitoriaTests/Ejercicio2_VitoriaTests.swift
@@ -0,0 +1,17 @@
+//
+//  Ejercicio2_VitoriaTests.swift
+//  Ejercicio2_VitoriaTests
+//
+//  Created by Vitoria Garcia on 28/11/24.
+//
+
+import Testing
+@testable import Ejercicio2_Vitoria
+
+struct Ejercicio2_VitoriaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
